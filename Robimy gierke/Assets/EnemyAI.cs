103f1693646c10ccfbe1e84f1cd71294202324a5
DkeyGitGud <dariuszkapustka@gmail.com>
2019-11-11 18:11:20 +0100

    after 14 3 of 3 / 36

--- Robimy gierke/Assets/EnemyAI.cs
+++ Robimy gierke/Assets/EnemyAI.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Collections;
 using UnityEngine;
 using Pathfinding;
 
@@ -46,7 +47,22 @@ public class EnemyAI : MonoBehaviour
         // Start a new path to the target position, return the result to the OnPathComplete method
         seeker.StartPath(transform.position, target.position, OnPathComplete);
 
+        StartCoroutine(UpdatePath());
         // WRITE SOME MORE
+    }
+
+    IEnumerator UpdatePath()
+    {
+        if (target == null)
+        {
+            //TODO: Insert a player search here.
+            yield return false;
+        }
+
+        seeker.StartPath(transform.position, target.position, OnPathComplete);
+
+        yield return new WaitForSeconds(1f / updateRate);
+        StartCoroutine(UpdatePath());
     }
 
     public void OnPathComplete(Path p)
@@ -59,4 +75,43 @@ public class EnemyAI : MonoBehaviour
         }
     }
 
+    private void FixedUpdate()
+    {
+        if (target == null)
+        {
+            //TODO: Insert a player search here.
+            return;
+        }
+
+        //TODO: Always look at player?
+
+        if (path == null)
+            return;
+
+        if (currentWaypoint >= path.vectorPath.Count)
+        {
+            if (pathIsEnded)
+                return;
+
+            Debug.Log("End of path reached.");
+            pathIsEnded = true;
+            return;
+        }
+        pathIsEnded = false;
+
+        //Direction to the next waypoint
+
+        Vector3 dir = (path.vectorPath[currentWaypoint] - transform.position).normalized;
+        dir *= speed * Time.fixedDeltaTime;
+
+        //Move the AI
+        rb.AddForce(dir, fMode);
+
+        float dist =  Vector3.Distance(transform.position, path.vectorPath[currentWaypoint]) ;
+        if (dist < nextWaypointDistance)
+        {
+            currentWaypoint++;
+            return;
+        }
+    }
 }
