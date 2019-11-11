f46c6b356420e440dd08b6fe4a6585fc23922d13
DkeyGitGud <dariuszkapustka@gmail.com>
2019-11-11 15:46:31 +0100

    after 12/36

--- Robimy gierke/Assets/GameMaster.cs
+++ Robimy gierke/Assets/GameMaster.cs
@@ -4,8 +4,32 @@ using UnityEngine;
 
 public class GameMaster : MonoBehaviour
 {
+    public static GameMaster gm;
+
+    void Start()
+    {
+        if (gm == null)
+        {
+            gm = GameObject.FindGameObjectWithTag("GM").GetComponent<GameMaster>();
+        }
+    }
+
+    public Transform playerPrefab;
+    public Transform spawnPoint;
+    public int spawnDelay = 2;
+
+    public IEnumerator RespawnPlayer()
+    {
+        Debug.Log("TODO: Add waiting for spawn sound");
+        yield return new WaitForSeconds(spawnDelay);
+
+        Instantiate(playerPrefab, spawnPoint.position, spawnPoint.rotation);
+        Debug.Log("TODO: Add Sprawn Particles");
+    }
+
     public static void KillPlayer(Player player)
     {
         Destroy(player.gameObject);
+        gm.StartCoroutine(gm.RespawnPlayer());
     }
 }
