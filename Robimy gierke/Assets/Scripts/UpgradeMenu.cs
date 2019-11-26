9b3d0b11e6a78d89b1d43ee53a411b560d222352
DkeyGitGud <dariuszkapustka@gmail.com>
2019-11-26 19:46:06 +0100

    Zaimplementowac liste upcycling ideas

--- Robimy gierke/Assets/Scripts/UpgradeMenu.cs
+++ Robimy gierke/Assets/Scripts/UpgradeMenu.cs
@@ -10,13 +10,27 @@ public class UpgradeMenu : MonoBehaviour
     private Text speedText;
 
     [SerializeField]
-    private float healthMultiplier = 1.3f;
+    private int healthAddAmount1 = 15;
+    [SerializeField]
+    private int healthAddAmount3 = 25;
+    [SerializeField]
+    private int healthAddAmount5 = 40;
 
     [SerializeField]
-    private float movementMultiplier = 1.2f;
+    private int movementAddAmount2 = 1;
+    [SerializeField]
+    private int movementAddAmount4 = 2;
 
     [SerializeField]
-    private int upgradeCost = 50;
+    private int upgradeCost1 = 10;
+    [SerializeField]
+    private int upgradeCost2 = 20;
+    [SerializeField]
+    private int upgradeCost3 = 30;
+    [SerializeField]
+    private int upgradeCost4 = 40;
+    [SerializeField]
+    private int upgradeCost5 = 50;
 
     private PlayerStats stats;
 
@@ -32,33 +46,81 @@ public class UpgradeMenu : MonoBehaviour
         speedText.text = "SPEED: " + stats.movementSpeed;
     }
 
-    public void UpgradeHealth()
+    public void UpgradeHealth1()
     {
-        if(GameMaster.Money < upgradeCost)
+        if(GameMaster.Money < upgradeCost1)
         {
             AudioManager.instance.PlaySound("NoMoney");
             return;
         }
 
-        stats.maxHealth = (int)(stats.maxHealth * healthMultiplier);
+        stats.maxHealth = stats.maxHealth + healthAddAmount1;
 
-        GameMaster.Money -= upgradeCost;
+        GameMaster.Money -= upgradeCost1;
         AudioManager.instance.PlaySound("Money");
 
         UpdateValues();
     }
 
-    public void UpgradeSpeed()
+    public void UpgradeSpeed2()
     {
-        if (GameMaster.Money < upgradeCost)
+        if (GameMaster.Money < upgradeCost2)
         {
             AudioManager.instance.PlaySound("NoMoney");
             return;
         }
 
-        stats.movementSpeed = Mathf.Round(stats.movementSpeed * movementMultiplier);
+        stats.movementSpeed = stats.movementSpeed + movementAddAmount2;
 
-        GameMaster.Money -= upgradeCost;
+        GameMaster.Money -= upgradeCost2;
+        AudioManager.instance.PlaySound("Money");
+
+        UpdateValues();
+    }
+
+    public void UpgradeHealth3()
+    {
+        if (GameMaster.Money < upgradeCost3)
+        {
+            AudioManager.instance.PlaySound("NoMoney");
+            return;
+        }
+
+        stats.maxHealth = stats.maxHealth + healthAddAmount3;
+
+        GameMaster.Money -= upgradeCost3;
+        AudioManager.instance.PlaySound("Money");
+
+        UpdateValues();
+    }
+
+    public void UpgradeSpeed4()
+    {
+        if (GameMaster.Money < upgradeCost4)
+        {
+            AudioManager.instance.PlaySound("NoMoney");
+            return;
+        }
+
+        stats.movementSpeed = stats.movementSpeed + movementAddAmount4;
+
+        GameMaster.Money -= upgradeCost4;
+        AudioManager.instance.PlaySound("Money");
+
+        UpdateValues();
+    }
+
+    public void UpgradeHealth5()
+    {
+        if (GameMaster.Money < upgradeCost5)
+        {
+            AudioManager.instance.PlaySound("NoMoney");
+            return;
+        }
+
+        stats.maxHealth = stats.maxHealth + healthAddAmount5;
+
+        GameMaster.Money -= upgradeCost5;
         AudioManager.instance.PlaySound("Money");
 
         UpdateValues();
